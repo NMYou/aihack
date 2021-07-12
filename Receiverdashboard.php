a7592452fda0422a78c9939ef8c2346ec978b216
NMYou <77392610+NMYou@users.noreply.github.com>
2021-07-12 18:13:58 +0530

    posted

--- Receiverdashboard.php
+++ Receiverdashboard.php
@@ -21,7 +21,7 @@
               <a class="navbar-brand" href="" ></a>
               <a class="navbar-brand"></a>
               <li class="nav-item active">
-                <a class="nav-link" href="Index.php" style="padding-left: 50%; padding-right: 1%;">Logout <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php" style="padding-left: 50%; padding-right: 1%;">Logout <span class="sr-only">(current)</span></a>
               </li>
             </ul>
           </div>
